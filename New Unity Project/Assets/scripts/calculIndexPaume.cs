3fe8bc7ae24d3de61795bf935ca15d2b61b98886
MrRedbloX <charles.cogoluegnes@gmail.com>
2020-02-11 15:49:44 +0100

    Add Unity Project

---
+++ New Unity Project/Assets/scripts/calculIndexPaume.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class calculIndexPaume : MonoBehaviour
+{
+	public Transform tr;
+	 
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = this.gameObject.transform;
+
+		
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+				print(tr.GetChild(0).gameObject.transform.GetChild(5).transform.position);
+
+    }
+}
