5ca9e221eb61d6b6de519e3412f9b4d55b29486d
HMaxime <you@example.com>
2020-05-01 13:58:21 +0200

    bug fix et ajout de délai lors de la transition entre les scènes

--- Unity/Assets/scripts/changeSceneScript.cs
+++ Unity/Assets/scripts/changeSceneScript.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class changeSceneScript : MonoBehaviour
-{
-    public void GoToScene(string nameScene){
-        SceneManager.LoadScene(nameScene);
+public class changeSceneScript : MonoBehaviour {
+    public void GoToScene (string nameScene) {
+        StartCoroutine(waitNSecondsAndgo (3,nameScene));
     }
+
+    IEnumerator waitNSecondsAndgo (int nbSeconds, string nameScene) {
+        yield return new WaitForSeconds (3);
+        SceneManager.LoadScene (nameScene);
+    }
+
 }
